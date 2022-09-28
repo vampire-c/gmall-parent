e8535c996b65bdb5ef17a6a44cd6dbd1249c6921
vampire-c <1477437516@qq.com>
2022-09-29 01:08:46 +0800

    @EnableSwagger3 @EnableMinio

--- service/service-product/src/main/java/com/atguigu/gmall/product/ProductMainApplication.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/ProductMainApplication.java
@@ -1,12 +1,17 @@
 package com.atguigu.gmall.product;
 
+import com.atguigu.gmall.common.config.minio.annotation.EnableMinio;
+import com.atguigu.gmall.common.config.swagger.annotation.EnableSwagger3;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
-// @EnableCircuitBreaker
-// @EnableDiscoveryClient
+// @EnableCircuitBreaker // 熔断降级
+// @EnableDiscoveryClient // 服务发现
 // @SpringBootApplication
+
+@EnableSwagger3
+@EnableMinio
 @MapperScan("com.atguigu.gmall.product.mapper")
 @SpringCloudApplication
 public class ProductMainApplication {
