1d33ce2c9cdeda769c5384ae044b584a23fe0b77
vampire-c <1477437516@qq.com>
2022-09-29 01:03:27 +0800

    Swagger注解添加

--- service/service-product/src/main/java/com/atguigu/gmall/product/controller/CategoryController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/CategoryController.java
@@ -7,31 +7,35 @@ import com.atguigu.gmall.product.entity.BaseCategory3;
 import com.atguigu.gmall.product.service.BaseCategory1Service;
 import com.atguigu.gmall.product.service.BaseCategory2Service;
 import com.atguigu.gmall.product.service.BaseCategory3Service;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = "平台属性列表接口")
+@RequestMapping("/admin/product")
 @RestController
-public class BaseCategory1Controller {
+public class CategoryController {
 
     @Autowired
-    BaseCategory1Service baseCategory1Service;
+    private BaseCategory1Service baseCategory1Service;
 
     @Autowired
-    BaseCategory2Service baseCategory2Service;
+    private BaseCategory2Service baseCategory2Service;
 
     @Autowired
-    BaseCategory3Service baseCategory3Service;
+    private BaseCategory3Service baseCategory3Service;
 
     /**
-     * 查询一级分类
-     *
      * @return
      */
-    @GetMapping("/admin/product/getCategory1")
+    @ApiOperation("查询一级分类")
+    @GetMapping("/getCategory1")
     public Result getCategory1() {
         List<BaseCategory1> list = this.baseCategory1Service.list();
         return Result.ok(list);
@@ -44,7 +48,8 @@ public class BaseCategory1Controller {
      * @param category_id
      * @return
      */
-    @GetMapping("/admin/product/getCategory2/{category_id}")
+    @ApiOperation("根据一级分类id,查询二级分类")
+    @GetMapping("/getCategory2/{category_id}")
     public Result getCategory2(@PathVariable Long category_id) {
         List<BaseCategory2> list = this.baseCategory2Service.getCategory2(category_id);
         return Result.ok(list);
@@ -56,7 +61,8 @@ public class BaseCategory1Controller {
      * @param category_id
      * @return
      */
-    @GetMapping("admin/product/getCategory3/{category_id}")
+    @ApiOperation("根据二级分类id,查询三级分类")
+    @GetMapping("/getCategory3/{category_id}")
     public Result getCategory3(@PathVariable Long category_id) {
         List<BaseCategory3> list = this.baseCategory3Service.getCategory3(category_id);
         return Result.ok(list);
