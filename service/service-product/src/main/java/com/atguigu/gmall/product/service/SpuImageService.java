ee537a4262de98930c81e1c783b8aecdca88616a
vampire-c <1477437516@qq.com>
2022-09-29 21:43:55 +0800

    查询SPU的图片列表

--- service/service-product/src/main/java/com/atguigu/gmall/product/service/SpuImageService.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/SpuImageService.java
@@ -3,6 +3,8 @@ package com.atguigu.gmall.product.service;
 import com.atguigu.gmall.product.entity.SpuImage;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
 * @author Anonymous
 * @description 针对表【spu_image(商品图片表)】的数据库操作Service
@@ -10,4 +12,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface SpuImageService extends IService<SpuImage> {
 
+    /**
+     * 查询SPU的图片列表
+     * @param spuId
+     */
+    List<SpuImage> getSpuImageList(Long spuId);
 }
--- service/service-product/src/main/java/com/atguigu/gmall/product/service/impl/SpuImageServiceImpl.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/impl/SpuImageServiceImpl.java
@@ -1,20 +1,38 @@
 package com.atguigu.gmall.product.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.atguigu.gmall.product.entity.SpuImage;
 import com.atguigu.gmall.product.service.SpuImageService;
 import com.atguigu.gmall.product.mapper.SpuImageMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
-* @author Anonymous
-* @description 针对表【spu_image(商品图片表)】的数据库操作Service实现
-* @createDate 2022-09-26 19:16:22
-*/
+ * @author Anonymous
+ * @description 针对表【spu_image(商品图片表)】的数据库操作Service实现
+ * @createDate 2022-09-26 19:16:22
+ */
 @Service
 public class SpuImageServiceImpl extends ServiceImpl<SpuImageMapper, SpuImage>
-    implements SpuImageService{
+        implements SpuImageService {
 
+    @Autowired
+    private SpuImageMapper spuImageMapper;
+
+    /**
+     * 查询SPU的图片列表
+     *
+     * @param spuId
+     */
+    @Override
+    public List<SpuImage> getSpuImageList(Long spuId) {
+        LambdaQueryWrapper<SpuImage> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(SpuImage::getSpuId, spuId);
+        return spuImageMapper.selectList(queryWrapper);
+    }
 }
 
 
