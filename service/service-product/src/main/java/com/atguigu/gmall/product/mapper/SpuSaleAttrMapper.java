969c68cdf6cb1684d36d724bf95c4ef59b3ba9d2
vampire-c <1477437516@qq.com>
2022-09-29 21:46:09 +0800

    查询销售属性的名和值

--- service/service-product/src/main/java/com/atguigu/gmall/product/mapper/SpuSaleAttrMapper.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/mapper/SpuSaleAttrMapper.java
@@ -2,6 +2,10 @@ package com.atguigu.gmall.product.mapper;
 
 import com.atguigu.gmall.product.entity.SpuSaleAttr;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
 * @author Anonymous
@@ -9,8 +13,15 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @createDate 2022-09-26 19:16:22
 * @Entity com.atguigu.gmall.product.entity.SpuSaleAttr
 */
+@Repository
 public interface SpuSaleAttrMapper extends BaseMapper<SpuSaleAttr> {
 
+    /**
+     * 查询销售属性的名和值
+     * @param spuId
+     * @return
+     */
+    List<SpuSaleAttr> getSpuSaleAttrList(@Param("spuId") Long spuId);
 }
 
 
