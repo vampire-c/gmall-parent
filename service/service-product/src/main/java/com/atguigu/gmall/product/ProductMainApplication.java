c28c72ba8fd28cb0264304fdf9887f6f9162c1dc
vampire-c <1477437516@qq.com>
2022-09-29 21:40:05 +0800

    @EnableTransactionManagement // 开启事务

--- service/service-product/src/main/java/com/atguigu/gmall/product/ProductMainApplication.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/ProductMainApplication.java
@@ -5,13 +5,15 @@ import com.atguigu.gmall.common.config.swagger.annotation.EnableSwagger3;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 // @EnableCircuitBreaker // 熔断降级
 // @EnableDiscoveryClient // 服务发现
 // @SpringBootApplication
 
-@EnableSwagger3
-@EnableMinio
+@EnableTransactionManagement // 开启事务
+@EnableSwagger3 // 开启Swagger
+@EnableMinio //开启文件上传
 @MapperScan("com.atguigu.gmall.product.mapper")
 @SpringCloudApplication
 public class ProductMainApplication {
