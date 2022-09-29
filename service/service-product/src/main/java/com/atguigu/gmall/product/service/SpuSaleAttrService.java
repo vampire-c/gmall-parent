a4a13124e58c80e9faea7022b11318d949a26c4c
vampire-c <1477437516@qq.com>
2022-09-29 21:44:58 +0800

    查询销售属性的名和值

--- service/service-product/src/main/java/com/atguigu/gmall/product/service/SpuSaleAttrService.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/SpuSaleAttrService.java
@@ -3,6 +3,8 @@ package com.atguigu.gmall.product.service;
 import com.atguigu.gmall.product.entity.SpuSaleAttr;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
 * @author Anonymous
 * @description 针对表【spu_sale_attr(spu销售属性)】的数据库操作Service
@@ -10,4 +12,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface SpuSaleAttrService extends IService<SpuSaleAttr> {
 
+    /**
+     * 查询销售属性的名和值
+     * @param spuId
+     * @return
+     */
+    List<SpuSaleAttr> getSpuSaleAttrList(Long spuId);
 }
--- service/service-product/src/main/java/com/atguigu/gmall/product/service/impl/SpuSaleAttrServiceImpl.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/impl/SpuSaleAttrServiceImpl.java
@@ -1,20 +1,37 @@
 package com.atguigu.gmall.product.service.impl;
 
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.atguigu.gmall.product.entity.SpuSaleAttr;
-import com.atguigu.gmall.product.service.SpuSaleAttrService;
 import com.atguigu.gmall.product.mapper.SpuSaleAttrMapper;
+import com.atguigu.gmall.product.service.SpuSaleAttrService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
-* @author Anonymous
-* @description 针对表【spu_sale_attr(spu销售属性)】的数据库操作Service实现
-* @createDate 2022-09-26 19:16:22
-*/
+ * @author Anonymous
+ * @description 针对表【spu_sale_attr(spu销售属性)】的数据库操作Service实现
+ * @createDate 2022-09-26 19:16:22
+ */
 @Service
 public class SpuSaleAttrServiceImpl extends ServiceImpl<SpuSaleAttrMapper, SpuSaleAttr>
-    implements SpuSaleAttrService{
+        implements SpuSaleAttrService {
 
+
+    @Autowired
+    private SpuSaleAttrMapper spuSaleAttrMapper;
+
+    /**
+     * 查询销售属性的名和值
+     *
+     * @param spuId
+     * @return
+     */
+    @Override
+    public List<SpuSaleAttr> getSpuSaleAttrList(Long spuId) {
+        return spuSaleAttrMapper.getSpuSaleAttrList(spuId);
+    }
 }
 
 
